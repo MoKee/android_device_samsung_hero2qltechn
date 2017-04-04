aaefb6f2725a4e3f539d200235f8782c7e7a667f
nian0114 <268078545@qq.com>
2017-04-05 01:00:28 +0800

    hero2qlte: remove tap2wake

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,7 +127,6 @@ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # CNE and DPM
 BOARD_USES_QCNE := true
