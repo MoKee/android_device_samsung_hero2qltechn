9ba2a086b6a782bcd7830012c041a3ec3917bc4a
nian0114 <268078545@qq.com>
2017-04-11 03:00:25 +0800

    hero2qlte: build files include boot.img to fix sensors

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -89,6 +89,7 @@ LOCAL_MODULE		:= init.qcom.bt.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.bt.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -96,6 +97,7 @@ LOCAL_MODULE		:= init.qcom.sensors.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.sensors.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -103,4 +105,5 @@ LOCAL_MODULE		:= init.qcom.syspart_fixup.sh
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.qcom.syspart_fixup.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
